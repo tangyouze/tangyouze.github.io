4ff6493de64db850603105462bb7a92e6642e272
Youze Tang <tangyouze@gmail.com>
2014-08-18 21:59:00 +0800

    auto commit

--- .r
+++ .r
@@ -5,8 +5,8 @@ filename=cc5.xml
 head -n 10 c.xml > $filename
 cat c.html >> $filename
 tail -n 10 c.xml >> $filename
-git add . 
 git add --all
+git add . 
 git commit  -m 'auto commit'
 git push > /dev/null 2>/dev/null&
 
